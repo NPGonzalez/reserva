2feb9b04e699b50bae11bf66e5145ac8981cad0d
NPGonzalez <naypili@gmail.com>
2020-10-13 22:22:57 -0300

    Se trabaja con el almacenamiento

--- src/index.js
+++ src/index.js
@@ -7,10 +7,20 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = movieSelect.value;
 
-
+const setMovieData = (movieIndex, moviePrice) => {
+    localStorage.setItem('selectMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 const updateSelectedCount = () => {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map(e => [...seats].indexOf(e));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+    console.log(seatsIndex);
+
+
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
@@ -21,6 +31,7 @@ const updateSelectedCount = () => {
 
 movieSelect.addEventListener('change', (e) => {
     ticketPrice = e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
 
